041efc63bd226db684b68c4e8c1b6ad89cb2646e
Christian <Christian_Eilers@gmx.net>
2018-03-11 16:06:54 +0100

    Kommentar-Anzahl SwipeRefreshLayout

--- app/src/main/java/christian/eilers/flibber/Home/HomeFragment.java
+++ app/src/main/java/christian/eilers/flibber/Home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
@@ -13,12 +14,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -52,6 +56,7 @@ public class HomeFragment extends Fragment {
     // Initialize views from layout file
     private void initializeViews() {
         recView = mainView.findViewById(R.id.recView);
+        refreshLayout = mainView.findViewById(R.id.refreshLayout);
         fab = mainView.findViewById(R.id.fab);
         progressBar = mainView.findViewById(R.id.progressBar);
         placeholder = mainView.findViewById(R.id.placeholder);
@@ -62,6 +67,12 @@ public class HomeFragment extends Fragment {
                 Intent intent_newNote = new Intent(getContext(), NoteCreateActivity.class);
                 getActivity().startActivity(intent_newNote);
                 getActivity().overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                adapter.notifyDataSetChanged();
             }
         });
     }
@@ -94,12 +105,11 @@ public class HomeFragment extends Fragment {
                 if (getItemCount() == 0) placeholder.setVisibility(View.VISIBLE);
                 else placeholder.setVisibility(View.GONE);
                 progressBar.setVisibility(View.GONE);
-                adapter.notifyDataSetChanged();
             }
 
             // Bind data from the database to the UI-Object
             @Override
-            public void onBindViewHolder(HomeFragment.NotesHolder holder, int position, final Note model) {
+            public void onBindViewHolder(final HomeFragment.NotesHolder holder, int position, final Note model) {
                 User user = users.get(model.getUserID());
                 holder.tv_username.setText(user.getName());
 
@@ -155,6 +165,17 @@ public class HomeFragment extends Fragment {
                         getActivity().overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                     }
                 });
+
+
+                holder.tv_comments.setText("...");
+                getSnapshots().getSnapshot(position).getReference().collection(COMMENTS).get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot documentSnapshots) {
+                                holder.tv_comments.setText(documentSnapshots.size()+"");
+                                refreshLayout.setRefreshing(false);
+                            }
+                        });
             }
 
             // Einmalige Zuweisung zum ViewHolder: NotesHolder
@@ -185,7 +206,7 @@ public class HomeFragment extends Fragment {
     public class NotesHolder extends RecyclerView.ViewHolder {
         View itemView;
         CircleImageView img_profile;
-        TextView tv_username, tv_title, tv_description, tv_datum;
+        TextView tv_username, tv_title, tv_description, tv_datum, tv_comments;
         ImageView img_note;
 
         public NotesHolder(View itemView) {
@@ -196,12 +217,14 @@ public class HomeFragment extends Fragment {
             tv_description = itemView.findViewById(R.id.description);
             tv_title = itemView.findViewById(R.id.title);
             tv_username = itemView.findViewById(R.id.username);
+            tv_comments = itemView.findViewById(R.id.comment_count);
             img_note = itemView.findViewById(R.id.image);
         }
     }
 
     private View mainView;
     private RecyclerView recView;
+    private SwipeRefreshLayout refreshLayout;
     private ProgressBar progressBar;
     private TextView placeholder;
     private FirestoreRecyclerAdapter adapter;
@@ -216,5 +239,6 @@ public class HomeFragment extends Fragment {
     private final String TIMESTAMP = "timestamp";
     private final String PROFILE = "profile";
     private final String NOTEID = "noteID";
+    private final String COMMENTS = "comments";
     private final int BUFFER = 10000; // Millisekunden // entspricht 10 Sekunden
 }
