c4bf8135907bb4fea12ee31798296d40304c0c4d
Christian <Christian_Eilers@gmx.net>
2018-03-15 21:48:04 +0100

    Finanzen Bilanz

--- app/src/main/java/christian/eilers/flibber/Home/FinanceFragment.java
+++ app/src/main/java/christian/eilers/flibber/Home/FinanceFragment.java
@@ -3,16 +3,22 @@ package christian.eilers.flibber.Home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -20,7 +26,9 @@ import java.util.HashMap;
 
 import christian.eilers.flibber.Models.User;
 import christian.eilers.flibber.R;
+import christian.eilers.flibber.Utils.GlideApp;
 import christian.eilers.flibber.Utils.LocalStorage;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FinanceFragment extends Fragment {
 
@@ -31,6 +39,7 @@ public class FinanceFragment extends Fragment {
         mainView= inflater.inflate(R.layout.fragment_finanzen, container, false);
         initializeViews();
         initializeVariables();
+        loadBilanz();
         return mainView;
     }
 
@@ -51,13 +60,58 @@ public class FinanceFragment extends Fragment {
 
     // Initialize variables
     private void initializeVariables() {
+        db = FirebaseFirestore.getInstance();
         storage = FirebaseStorage.getInstance().getReference();
         groupID = LocalStorage.getGroupID(getContext());
         users = ((HomeActivity) getActivity()).getUsers();
     }
 
     private void loadBilanz() {
+        Query query = db.collection(GROUPS).document(groupID)
+                .collection(USERS).orderBy(MONEY);
 
+        FirestoreRecyclerOptions<User> options = new FirestoreRecyclerOptions.Builder<User>()
+                .setQuery(query, User.class)
+                .build();
+
+        adapterBilanz = new FirestoreRecyclerAdapter<User, UserHolder>(options) {
+            @NonNull
+            @Override
+            public UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_finanzen_user, parent, false);
+                return new UserHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull UserHolder holder, int position, @NonNull User model) {
+                holder.tv_username.setText(model.getName());
+                holder.tv_money.setText(model.getMoney() + " \u20ac");
+                // PROFILE PICTURE
+                if(model.getPicPath() != null)
+                    GlideApp.with(getContext())
+                            .load(storage.child(PROFILE).child(model.getPicPath()))
+                            .dontAnimate()
+                            .placeholder(R.drawable.profile_placeholder)
+                            .into(holder.img_profile);
+            }
+        };
+
+        recBilanz.setLayoutManager(new LinearLayoutManager(getContext()));
+        recBilanz.setAdapter(adapterBilanz);
+
+    }
+
+    public class UserHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView img_profile;
+        TextView tv_username, tv_money;
+
+        public UserHolder(View itemView) {
+            super(itemView);
+            img_profile = itemView.findViewById(R.id.profile_image);
+            tv_username = itemView.findViewById(R.id.username);
+            tv_money = itemView.findViewById(R.id.money);
+        }
     }
 
     @Override
@@ -75,6 +129,7 @@ public class FinanceFragment extends Fragment {
     }
 
     private StorageReference storage;
+    private FirebaseFirestore db;
     private String groupID;
     private HashMap<String, User> users;
 
@@ -83,4 +138,11 @@ public class FinanceFragment extends Fragment {
     private RecyclerView recBilanz, recVerlauf;
     private FirestoreRecyclerAdapter adapterBilanz, adapterVerlauf;
     private FloatingActionButton fab;
+
+    private final String GROUPS = "groups";
+    private final String USERS = "users";
+    private final String FINANCES = "finances";
+    private final String TIMESTAMP = "timestamp";
+    private final String MONEY = "money";
+    private final String PROFILE = "profile";
 }
