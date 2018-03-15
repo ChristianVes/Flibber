85f5e83eee292d36da5e7bacd47b08910d7bc832
Christian <Christian_Eilers@gmx.net>
2018-03-15 21:29:00 +0100

    Finanzeneintrag Adapter

--- app/src/main/java/christian/eilers/flibber/Home/FinanceEntryActivity.java
+++ app/src/main/java/christian/eilers/flibber/Home/FinanceEntryActivity.java
@@ -2,8 +2,29 @@ package christian.eilers.flibber.Home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import christian.eilers.flibber.Adapter.BeteiligteAdapter;
+import christian.eilers.flibber.Adapter.BezahlerAdapter;
+import christian.eilers.flibber.Models.User;
 import christian.eilers.flibber.R;
+import christian.eilers.flibber.Utils.LocalStorage;
 
 public class FinanceEntryActivity extends AppCompatActivity {
 
@@ -11,5 +32,85 @@ public class FinanceEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance_entry);
+        initializeViews();
+        initializeVariables();
     }
+
+    private void initializeViews() {
+        toolbar = findViewById(R.id.toolbar);
+        et_article = findViewById(R.id.input_article);
+        et_description = findViewById(R.id.input_description);
+        et_price = findViewById(R.id.input_price);
+        rec_beteiligte = findViewById(R.id.listBeteiligte);
+        rec_bezahler = findViewById(R.id.listBezahler);
+        rec_beteiligte = findViewById(R.id.listBeteiligte);
+
+        rec_bezahler.setHasFixedSize(true);
+        rec_bezahler.setLayoutManager(new LinearLayoutManager(this));
+        rec_beteiligte.setHasFixedSize(true);
+        rec_beteiligte.setLayoutManager(new LinearLayoutManager(this));
+
+        setSupportActionBar(toolbar); // Toolbar als Actionbar setzen
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // Titel der Actionbar ausblenden
+    }
+
+    // Initialize variables
+    private void initializeVariables() {
+        userID = LocalStorage.getUserID(this);
+        groupID = LocalStorage.getGroupID(this);
+        db = FirebaseFirestore.getInstance();
+
+        db.collection(GROUPS).document(groupID).collection(USERS).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot documentSnapshots) {
+                retrieveUsers(documentSnapshots);
+                ArrayList<User> userList = new ArrayList<>(users.values());
+                adapter_bezahler = new BezahlerAdapter(userList, userID);
+                adapter_beteiligte = new BeteiligteAdapter(userList);
+                rec_bezahler.setAdapter(adapter_bezahler);
+                rec_beteiligte.setAdapter(adapter_beteiligte);
+            }
+        });
+    }
+
+    // Erzeugt eine Userliste mithilfe eines Snapshots aus der Datenbank
+    private void retrieveUsers(QuerySnapshot documentSnapshots) {
+        HashMap<String, User> userHashMap = new HashMap<>();
+        for(DocumentSnapshot doc : documentSnapshots) {
+            User user = doc.toObject(User.class);
+            userHashMap.put(user.getUserID(), user);
+        }
+        users = (HashMap<String, User>) userHashMap.clone();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_finanzen_eintrag, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                // TODO <->
+                Toast.makeText(this, "Noch nicht möglich...", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private String userID, groupID;
+    private FirebaseFirestore db;
+    private HashMap<String, User> users;
+    private BezahlerAdapter adapter_bezahler;
+    private BeteiligteAdapter adapter_beteiligte;
+
+    private Toolbar toolbar;
+    private EditText et_price, et_article, et_description;
+    private RecyclerView rec_bezahler, rec_beteiligte;
+
+    private final String GROUPS = "groups";
+    private final String USERS = "users";
 }
--- app/src/main/java/christian/eilers/flibber/Home/FinanceFragment.java
+++ app/src/main/java/christian/eilers/flibber/Home/FinanceFragment.java
@@ -1,14 +1,26 @@
 package christian.eilers.flibber.Home;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+
+import christian.eilers.flibber.Models.User;
 import christian.eilers.flibber.R;
+import christian.eilers.flibber.Utils.LocalStorage;
 
 public class FinanceFragment extends Fragment {
 
@@ -16,8 +28,59 @@ public class FinanceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_finanzen, container, false);
-
-        return view;
+        mainView= inflater.inflate(R.layout.fragment_finanzen, container, false);
+        initializeViews();
+        initializeVariables();
+        return mainView;
     }
+
+    private void initializeViews() {
+        toolbar = mainView.findViewById(R.id.toolbar);
+        recBilanz = mainView.findViewById(R.id.recProfils);
+        recVerlauf = mainView.findViewById(R.id.recVerlauf);
+        fab = mainView.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().startActivity(new Intent(getContext(), FinanceEntryActivity.class));
+                getActivity().overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+    }
+
+    // Initialize variables
+    private void initializeVariables() {
+        storage = FirebaseStorage.getInstance().getReference();
+        groupID = LocalStorage.getGroupID(getContext());
+        users = ((HomeActivity) getActivity()).getUsers();
+    }
+
+    private void loadBilanz() {
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(adapterBilanz != null) adapterBilanz.startListening();
+        if(adapterVerlauf != null) adapterVerlauf.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(adapterBilanz != null) adapterBilanz.stopListening();
+        if(adapterVerlauf != null) adapterVerlauf.stopListening();
+    }
+
+    private StorageReference storage;
+    private String groupID;
+    private HashMap<String, User> users;
+
+    private View mainView;
+    private Toolbar toolbar;
+    private RecyclerView recBilanz, recVerlauf;
+    private FirestoreRecyclerAdapter adapterBilanz, adapterVerlauf;
+    private FloatingActionButton fab;
 }
