6f0f7753f93efc82309e87ad196ad920465b1161
Christian <christian_eilers@gmx.net>
2018-03-24 23:15:09 +0100

    channelIDS

--- app/src/main/java/christian/eilers/flibber/Utils/MyFirebaseMessagingService.java
+++ app/src/main/java/christian/eilers/flibber/Utils/MyFirebaseMessagingService.java
@@ -1,14 +1,19 @@
 package christian.eilers.flibber.Utils;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import christian.eilers.flibber.Home.HomeActivity;
 import christian.eilers.flibber.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -19,16 +24,32 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         if (remoteMessage.getData().size() == 0) return;
         String articleName = remoteMessage.getData().get("name");
+        String channelID = getString(R.string.fcm_fallback_notification_channel_label);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent, PendingIntent.FLAG_ONE_SHOT);
+
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notiBuilder = new NotificationCompat.Builder(this)
+        NotificationCompat.Builder notiBuilder = new NotificationCompat.Builder(this, channelID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(articleName)
                 .setContentText("Zur Einkaufsliste hinzugefügt")
-                .setSound(defaultSoundUri);
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelID,
+                    "Flibber",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         notificationManager.notify((int) System.currentTimeMillis(), notiBuilder.build());
     }
 }
