53159c089cb7b28e73758bb947177da2b4cd8c21
Christian <christian_eilers@gmx.net>
2018-03-25 12:54:14 +0200

    notification stack v1

--- app/src/main/java/christian/eilers/flibber/Utils/MyFirebaseMessagingService.java
+++ app/src/main/java/christian/eilers/flibber/Utils/MyFirebaseMessagingService.java
@@ -3,6 +3,7 @@ package christian.eilers.flibber.Utils;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
@@ -13,10 +14,14 @@ import android.support.v4.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.ArrayList;
+
 import christian.eilers.flibber.Home.HomeActivity;
 import christian.eilers.flibber.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public ArrayList<String> messages = new ArrayList<>();
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -26,17 +31,24 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         String articleName = remoteMessage.getData().get("name");
         String channelID = getString(R.string.fcm_fallback_notification_channel_label);
 
+        if (messages != null) messages.add(articleName);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        for(int i = 0; i < messages.size(); i++) inboxStyle.addLine(messages.get(i));
+
         Intent intent = new Intent(this, HomeActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent, PendingIntent.FLAG_ONE_SHOT);
 
-
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
         NotificationCompat.Builder notiBuilder = new NotificationCompat.Builder(this, channelID)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(articleName)
-                .setContentText("Zur Einkaufsliste hinzugefügt")
+                .setSmallIcon(R.drawable.recipes_book)
+                .setContentTitle("Einkaufsliste")
+                .setAutoCancel(true)
+                .setStyle(inboxStyle)
                 .setSound(defaultSoundUri)
+                .setDeleteIntent(PendingIntent.getBroadcast(this,101,new Intent(this, NotificationDismissReceiver.class),PendingIntent.FLAG_CANCEL_CURRENT))
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
@@ -51,5 +63,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
 
         notificationManager.notify((int) System.currentTimeMillis(), notiBuilder.build());
+    }
+
+
+    public class NotificationDismissReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (messages != null) messages.clear();
+        }
     }
 }
