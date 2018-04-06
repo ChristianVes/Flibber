a9255dda7cbbf5bf856ec10299c4d67de79150f9
Chris <Christian_Eilers@gmx.net>
2018-04-06 14:59:39 +0200

    DeviceToken - Deletion

--- app/src/main/java/christian/eilers/flibber/Profil/ProfilActivity.java
+++ app/src/main/java/christian/eilers/flibber/Profil/ProfilActivity.java
@@ -3,6 +3,7 @@ package christian.eilers.flibber.Profil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -10,12 +11,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.util.HashMap;
@@ -103,28 +111,52 @@ public class ProfilActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_logout:
-                // TODO: Delete devicetoken in every group
-                // Delete the DeviceToken from the current User
-                Map<String,Object> devicetoken = new HashMap<>();
-                devicetoken.put(DEVICETOKEN, FieldValue.delete());
-                FirebaseFirestore.getInstance().collection(USERS).document(auth.getCurrentUser().getUid()).update(devicetoken)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                auth.signOut(); // sign out the current user
-                                // delete local data
-                                LocalStorage.setData(ProfilActivity.this, null, null, null, null);
-                                // switch to LoginActivity
-                                Intent login = new Intent(ProfilActivity.this, LoginActivity.class);
-                                startActivity(login);
-                                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                                finish();
-                            }
-                        });
+                logout();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    // Log the current user out && Delete DeviceTokens in database
+    private void logout() {
+        progressBar.setVisibility(View.VISIBLE);
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Delete the DeviceToken from the current User
+        final Map<String,Object> map_devicetoken = new HashMap<>();
+        map_devicetoken.put(DEVICETOKEN, FieldValue.delete());
+        // User-ID
+        final String userID = auth.getCurrentUser().getUid();
+        // Get a new write batch
+        final WriteBatch batch = db.batch();
+        // Delete DeviceToken from User
+        DocumentReference doc_user = db.collection(USERS).document(userID);
+        batch.update(doc_user, map_devicetoken);
+        // Delete DeviceToken from user in each of his groups
+        db.collection(USERS).document(userID).collection(GROUPS).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                    DocumentReference doc_user = db.collection(GROUPS).document(document.getId()).collection(USERS).document(userID);
+                    batch.update(doc_user, map_devicetoken);
+                }
+                // Commit DeviceToken-Deletions and log out the user
+                batch.commit().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        progressBar.setVisibility(View.GONE);
+                        auth.signOut(); // sign out the current user
+                        // delete local data
+                        LocalStorage.setData(ProfilActivity.this, null, null, null, null);
+                        // switch to LoginActivity
+                        Intent login = new Intent(ProfilActivity.this, LoginActivity.class);
+                        startActivity(login);
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                        finish();
+                    }
+                });
+            }
+        });
     }
 
     // Allgemeine Einstellungen für Bottom Navigation View
